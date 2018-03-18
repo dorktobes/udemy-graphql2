9e7f29c28b61b98b897c37953662708ddf470e67
Toby Hoshman <t.hoshman@gmail.com>
2018-03-18 16:05:46 -0700

    added apollo query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
 
-  render () {
-    <div>
-      SongList
-    </div
+  renderSongs() {
+    return this.props.data.songs.map( song => {
+      return (
+        <li>
+        {song.title}
+        </li>
+        )
+    })
   }
-};
+
+  render () {
+    if (this.props.data.loading) {
+      return <div>loading...</div>
+    }
+
+  	return (
+    <div>
+      {this.renderSongs()}
+    </div>
+    )
+  }
+}
+
+const query = gql`
+{
+  songs {
+    title
+  }
+}
+`;
 
 
 
 
-export default SongList;
+export default graphql(query)(SongList);
--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList'
 
 const client = new ApolloClient({});
 
@@ -9,7 +10,7 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
