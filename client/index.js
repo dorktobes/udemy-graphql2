4c78d612a050d354b6f8134046586a7c96964e11
Toby Hoshman <t.hoshman@gmail.com>
2018-03-28 14:17:22 -0700

    connecterd graphql query in SongDetail component

--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
-
+import SongDetail from './components/SongDetail';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
@@ -18,6 +19,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
