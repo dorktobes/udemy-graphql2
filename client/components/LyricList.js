1381bfb98ec5cb6b9e87267503a8e42657baba18
Toby Hoshman <t.hoshman@gmail.com>
2018-03-29 08:26:15 -0700

    added lyric list componenet and conneced it to songdetails component

---
+++ client/components/LyricList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class LyricList extends Component {
+
+  renderLyrics() {
+    return this.props.lyrics.map(({ content, id}) => {
+      return (
+      <li 
+        key={id}
+        className="collection-item"
+      >
+        {content}
+      </li>)
+    })
+  }
+  render() {
+    return (
+      <ul className="collection">
+        {this.renderLyrics()}
+      </ul>);
+  }
+}
+
+export default LyricList;
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,13 +2,13 @@ import React, { Component} from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricList from './LyricList';
 import LyricCreate from './LyricCreate';
 
 class SongDetail extends Component {
 
   render() {
     const { song } = this.props.data;
-
     if (!song) {
       return <div>Loading...</div>
     }
@@ -16,7 +16,8 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-        <LyricCreate />
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     )
   }
